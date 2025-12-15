9fde91aa0105e9e339915929642e53df374343d3
Nguyễn Bá Nhật Hoàng <hoangmamg1234@gmail.com>
2025-12-16 00:44:03 +0700

    Update test4.py

--- test4.py
+++ test4.py
@@ -11,7 +11,7 @@ from pathlib import Path
 # CHỈ CẦN SỬA 2 DÒNG NÀY MỖI LẦN CHẠY
 # ===================================================================
 INPUT_JSON_FILES = [
-    "logs/momcts/bi_kp/nhv_runtime_20/v30/samples/samples_1~300.json",
+    "logs/meoh/bi_kp/nhv_runtime/v1/samples/samples_1~300.json",
     #"logs/nsga2/tsp_semo/nhv_runtime/v1/samples/samples_301~600.json",
     # "test/samples_301~600.json",
 ]
